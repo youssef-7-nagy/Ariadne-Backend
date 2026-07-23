8d6a331c8045f57cd1dd571302847593eb38def5
Youssef Nagy <youssefn899@gmail.com>
2026-07-23 18:12:13 +0300

    feat: link uploaded main image as poster thumbnail for embedded videos

--- services/media.service.js
+++ services/media.service.js
@@ -2,59 +2,75 @@ const { optimizeCoverImage } = require('./imageOptimizer');
 
 const processMedia = async (files, body) => {
   const media = [];
-  
-  if (files && files['media']) {
-    const mainFile = files['media'][0];
-    let mainUrl = `/uploads/${mainFile.filename}`;
 
-    // If main uploaded file is an image, optimize it via Sharp to WebP
+  let mainImageOptUrl = undefined;
+  let videoThumbOptUrl = undefined;
+
+  // Process video thumbnail if uploaded
+  if (files && files['videoThumbnail']) {
+    const thumbFile = files['videoThumbnail'][0];
+    console.log(`[MediaService] Optimizing video thumbnail: ${thumbFile.originalname}`);
+    videoThumbOptUrl = await optimizeCoverImage(thumbFile.filename);
+  }
+
+  // Process main uploaded file if provided
+  const mainFile = files && files['media'] ? files['media'][0] : null;
+  let mainUrl = undefined;
+  if (mainFile) {
+    mainUrl = `/uploads/${mainFile.filename}`;
     if (mainFile.mimetype.startsWith('image/')) {
       console.log(`[MediaService] Optimizing main image file: ${mainFile.originalname}`);
-      mainUrl = await optimizeCoverImage(mainFile.filename);
+      mainImageOptUrl = await optimizeCoverImage(mainFile.filename);
+      mainUrl = mainImageOptUrl;
+    }
+  }
+
+  // 1. If embed URL is provided (Bunny.net, YouTube, etc.)
+  if (body.embedUrl && body.embedUrl.trim()) {
+    let finalEmbedUrl = body.embedUrl.trim();
+    const srcMatch = finalEmbedUrl.match(/src=["']([^"']+)["']/);
+    if (srcMatch) {
+      finalEmbedUrl = srcMatch[1];
     }
 
-    let thumbnailUrl = undefined;
-    if (files['videoThumbnail']) {
-      const thumbFile = files['videoThumbnail'][0];
-      console.log(`[MediaService] Optimizing video thumbnail: ${thumbFile.originalname}`);
-      thumbnailUrl = await optimizeCoverImage(thumbFile.filename);
-    }
+    console.log(`[MediaService] Processing embed URL: ${finalEmbedUrl}`);
 
+    // Poster priority for embed: explicit videoThumbnail -> main uploaded image
+    const posterUrl = videoThumbOptUrl || mainImageOptUrl;
+
+    media.push({
+      type: 'embed',
+      url: finalEmbedUrl,
+      ...(posterUrl && { thumbnailUrl: posterUrl }),
+      isFeatured: true,
+      order: 0
+    });
+
+    // If main image was also uploaded, add it as a secondary gallery image item
+    if (mainImageOptUrl) {
+      media.push({
+        type: 'image',
+        url: mainImageOptUrl,
+        public_id: mainFile ? mainFile.filename : '',
+        resource_type: 'image',
+        isFeatured: false,
+        order: 1
+      });
+    }
+  }
+  // 2. Otherwise if main file was uploaded (image or video)
+  else if (mainFile) {
     const type = body.mediaType || (mainFile.mimetype.startsWith('video/') ? 'video' : 'image');
-
     console.log(`[MediaService] Processed uploaded file: ${mainFile.originalname} as ${type} -> URL: ${mainUrl}`);
+
+    const posterUrl = videoThumbOptUrl;
 
     media.push({
       type: type,
       url: mainUrl,
       public_id: mainFile.filename,
       resource_type: mainFile.mimetype.startsWith('video/') ? 'video' : 'image',
-      ...(thumbnailUrl && { thumbnailUrl }),
-      isFeatured: true,
-      order: 0
-    });
-  } else if (body.embedUrl) {
-    let finalEmbedUrl = body.embedUrl;
-    
-    // Extract src from iframe string if provided
-    const srcMatch = finalEmbedUrl.match(/src=["']([^"']+)["']/);
-    if (srcMatch) {
-      finalEmbedUrl = srcMatch[1];
-    }
-    
-    console.log(`[MediaService] Processing embed URL: ${finalEmbedUrl}`);
-    
-    let thumbnailUrl = undefined;
-    if (files && files['videoThumbnail']) {
-      const thumbFile = files['videoThumbnail'][0];
-      console.log(`[MediaService] Optimizing video thumbnail for embed: ${thumbFile.originalname}`);
-      thumbnailUrl = await optimizeCoverImage(thumbFile.filename);
-    }
-    
-    media.push({
-      type: 'embed',
-      url: finalEmbedUrl,
-      ...(thumbnailUrl && { thumbnailUrl }),
+      ...(posterUrl && { thumbnailUrl: posterUrl }),
       isFeatured: true,
       order: 0
     });
