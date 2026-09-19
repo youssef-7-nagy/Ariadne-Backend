198a665da3eb84487eca51739dd4674ae9f780ef
Youssef Nagy <youssefn899@gmail.com>
2026-09-19 03:30:16 +0300

    Fix youtubeUrl not being cleared in db when externalLink is emptied

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -135,7 +135,7 @@ exports.createProject = async (req, res) => {
       description, date,
       clientName,
       externalLink,
-      youtubeUrl: youtubeUrl || undefined,
+      youtubeUrl: youtubeUrl || '',
       mediaType: mediaType || 'video',
       tags: tags ? tags.split(',').map(t => t.trim()).filter(Boolean) : [],
       media,
@@ -161,7 +161,7 @@ exports.updateProject = async (req, res) => {
       description, date,
       clientName,
       externalLink,
-      youtubeUrl: youtubeUrl || undefined,
+      youtubeUrl: youtubeUrl || '',
       mediaType: mediaType || 'video',
       tags: tags ? tags.split(',').map(t => t.trim()).filter(Boolean) : []
     };
